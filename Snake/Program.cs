04907bad6775fe2a1b84d89e644e9b8817800ba6
German <german3113636@gmail.com>
2020-03-04 13:08:42 +0300

    6

--- Snake/Program.cs
+++ Snake/Program.cs
@@ -15,7 +15,26 @@ namespace Snake
 
             p2.Draw();
 
-          
+            list<int> numlist = new list<int>();
+            numlist.add(0);
+            numlist.add(1);
+            numlist.add(2);
+
+            int x = numlist[0];
+            int y = numlist[1];
+            int z = numlist[2];
+
+            foreach (int i in numlist)
+            {
+                Console.WriteLine(i);
+            }
+
+            numlist.RemuveAt(0);
+
+            list<point> plist = new list<point>();
+            plist.Add(p1);
+            plist.Add(p2);
+
             Console.ReadLine ();
              
             
